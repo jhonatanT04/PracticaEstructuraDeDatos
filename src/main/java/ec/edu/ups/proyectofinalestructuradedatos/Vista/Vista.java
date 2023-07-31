4e84c6fe341bc2258334acba5554606bbee95714
venot <venotacu@gmail.com">
2023-07-30 21:36:02 -0500

    Mi parte(Jhonatan)

--- src/main/java/ec/edu/ups/proyectofinalestructuradedatos/Vista/Vista.java
+++ src/main/java/ec/edu/ups/proyectofinalestructuradedatos/Vista/Vista.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 public class Vista {
     private Scanner teclado;
     private ArbolContactos arbol;
-    //
+    //aaa
     public Vista() {
         this.teclado = new Scanner(System.in);
     }
